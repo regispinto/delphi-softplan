21ecf69294182054b7325379d43e447996a8f9ea
Régis Silveira Pinto <regispinto65@gmail.com>
2022-08-19 14:36:08 -0300

    feat: Criado layout inicial do formulário principal

--- uMaster.pas
+++ uMaster.pas
@@ -3,21 +3,30 @@ unit uMaster;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
-  System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, FireDAC.Stan.Intf, FireDAC.Stan.Option,
-  FireDAC.Stan.Param, FireDAC.Stan.Error, FireDAC.DatS, FireDAC.Phys.Intf,
-  FireDAC.DApt.Intf, FireDAC.Stan.Async, FireDAC.DApt, Data.DB, Vcl.Grids,
-  Vcl.DBGrids, Vcl.ExtCtrls, Vcl.ComCtrls, FireDAC.Comp.DataSet,
-  FireDAC.Comp.Client, Vcl.StdCtrls, Vcl.Buttons;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes,
+  Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Data.DB, Vcl.Grids, Vcl.DBGrids,
+  Vcl.ExtCtrls, Vcl.ComCtrls, Vcl.StdCtrls, Vcl.Buttons,
+
+  FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Param, FireDAC.Stan.Error,
+  FireDAC.DatS, FireDAC.Phys.Intf, FireDAC.DApt.Intf, FireDAC.Stan.Async, FireDAC.DApt,
+  FireDAC.Comp.DataSet, FireDAC.Comp.Client,
+
+  System.Threading, Vcl.Imaging.jpeg, Vcl.Imaging.pngimage;
 
 type
   TfMaster = class(TForm)
-    dsLogs: TDataSource;
-    StatusBar1: TStatusBar;
-    Panel1: TPanel;
-    DBGrid1: TDBGrid;
-    BitBtn1: TBitBtn;
+    Image: TImage;
+    pnlMaster: TPanel;
+    gbxLink: TGroupBox;
+    Edit1: TEdit;
+    gbxOptions: TGroupBox;
+    btnStart: TBitBtn;
+    btnDisplayMessage: TBitBtn;
+    btnStop: TBitBtn;
+    btnClose: TBitBtn;
+    btnDisplayHistory: TBitBtn;
+    pnlProgressBar: TPanel;
+    ProgressBar: TProgressBar;
     procedure FormCreate(Sender: TObject);
     procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
     procedure BitBtn1Click(Sender: TObject);
@@ -45,7 +54,7 @@ end;
 
 procedure TfMaster.FormCreate(Sender: TObject);
 begin
-  //
+  pnlProgressBar.Visible := False;
 end;
 
 procedure TfMaster.FormKeyDown(Sender: TObject; var Key: Word;
