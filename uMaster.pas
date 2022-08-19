c73687f62725a8cc2c760b303e2b4ae1a0f5bf28
Régis Silveira Pinto <regispinto65@gmail.com>
2022-08-19 18:17:59 -0300

    feat: Finalizado o layout final/funcionalidades iniciais do formulário principal

--- uMaster.pas
+++ uMaster.pas
@@ -15,10 +15,9 @@ uses
 
 type
   TfMaster = class(TForm)
-    Image: TImage;
     pnlMaster: TPanel;
     gbxLink: TGroupBox;
-    Edit1: TEdit;
+    edtLink: TEdit;
     gbxOptions: TGroupBox;
     btnStart: TBitBtn;
     btnDisplayMessage: TBitBtn;
@@ -28,16 +27,26 @@ type
     pnlProgressBar: TPanel;
     ProgressBar: TProgressBar;
     procedure FormCreate(Sender: TObject);
-    procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
-    procedure BitBtn1Click(Sender: TObject);
+    procedure btnStartClick(Sender: TObject);
+    procedure btnDisplayMessageClick(Sender: TObject);
+    procedure btnStopClick(Sender: TObject);
+    procedure btnDisplayHistoryClick(Sender: TObject);
+    procedure btnCloseClick(Sender: TObject);
+    procedure edtLinkChange(Sender: TObject);
+
   private
     { Private declarations }
+
+    procedure OnOffOptions;
   public
     { Public declarations }
   end;
 
 var
   fMaster: TfMaster;
+  bLinkInformation: Boolean;
+  bInProgress: Boolean;
+  bMessage: Boolean;
 
 implementation
 
@@ -45,23 +54,66 @@ uses uDM;
 
 {$R *.dfm}
 
-procedure TfMaster.BitBtn1Click(Sender: TObject);
-begin
-  // fdQueryLogs.SQL.Clear;
-  // fdQueryLogs.SQL.Add('select * from logdonwload');
-  // fdQueryLogs.Open();
-end;
-
 procedure TfMaster.FormCreate(Sender: TObject);
 begin
   pnlProgressBar.Visible := False;
+
+  bLinkInformation := False;
+  bInProgress := False;
+  bMessage := False;
+
+  OnOffOptions;
 end;
 
-procedure TfMaster.FormKeyDown(Sender: TObject; var Key: Word;
-  Shift: TShiftState);
+procedure TfMaster.btnStartClick(Sender: TObject);
 begin
-  if Key = VK_ESCAPE then
-    Self.Close;
+  bInProgress := True;
+
+  OnOffOptions;
 end;
+
+procedure TfMaster.btnDisplayMessageClick(Sender: TObject);
+begin
+  bMessage := bMessage = False;
+
+  OnOffOptions;
+end;
+
+procedure TfMaster.btnStopClick(Sender: TObject);
+begin
+    bInProgress := False;
+
+  OnOffOptions;
+end;
+
+procedure TfMaster.btnDisplayHistoryClick(Sender: TObject);
+begin
+  // 4
+end;
+
+procedure TfMaster.btnCloseClick(Sender: TObject);
+begin
+  Application.Terminate;
+end;
+
+procedure TfMaster.edtLinkChange(Sender: TObject);
+begin
+  bLinkInformation := edtLink.Text <> EmptyStr;
+
+  OnOffOptions;
+end;
+
+procedure TfMaster.OnOffOptions;
+begin
+  gbxLink.Enabled           := not bInProgress;
+
+  btnStart.Enabled          := bLinkInformation and (not bInProgress);
+  btnDisplayMessage.Enabled := bInProgress;
+  btnStop.Enabled           := bInProgress;
+  btnDisplayHistory.Enabled := not bInProgress;
+
+  pnlProgressBar.Visible    := bInProgress and bMessage;
+end;
+
 
 end.
